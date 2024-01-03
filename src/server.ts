dce9f00aa1c69b934d790738d8dcbf9f151151d1
Gabriel Gesser <gabrielgesser@MacBook-Pro-de-Gabriel.local>
2024-01-02 22:13:54 -0300

    feat(gabriel): Configurando eslint ,alias de importação e env

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import { app } from "./app";
+import { env } from '../env'
+import { app } from './app'
 
-app. listen({
- host: '0.0.0.0',
- port: 3333,
-}).then(() => {
-  console.log("🚀 Running on port 3333");
-});
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('🚀 Running on port 3333')
+  })
